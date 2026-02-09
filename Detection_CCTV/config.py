c3af1e28270e86bc9e4d92f98d55798c22ff7350
GloryCiel <tgvae1122@gmail.com>
2026-02-09 14:39:14 +0900

    typo correction

--- Detection_CCTV/config.py
+++ Detection_CCTV/config.py
@@ -21,4 +21,4 @@ class AppConfig:
     # [변경됨] YOLOv8n -> YOLO26n (Ultralytics 최신 모델 적용)
     # yolo26n.pt: Nano 버전 (M1 Mac CPU 환경에 최적화됨)
     # 실행 시 자동으로 Ultralytics 서버에서 다운로드됩니다.
-    AI_MODEL_PATH: str = 'Detaction_CCTV/yolo26n.pt'
+    AI_MODEL_PATH: str = 'Detection_CCTV/yolo26n.pt'
